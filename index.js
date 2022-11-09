be6c337648bf0c2fd8be3113daf535d5205715b2
xlsandlx <luquilu_2009@hotmail.com>
2022-11-08 22:21:54 -0300

    segunda pre entrega

--- index.js
+++ index.js
@@ -1,51 +1,115 @@
-let buying = true,
+let compra = true,
     total = 0,
-    discount = 0
+    descuento = 0
 
-const getDiscount = total => {
+    let puma = {
+        precio: 5000,
+        stock: 2
+    }
+    
+    let adidas = {
+        precio: 8000,
+        stock: 2
+    }
+    
+    let jordan = {
+        precio: 10000,
+        stock: 2
+    }
+    
+    let nike = {
+        precio: 7500,
+        stock: 2
+    }
+
+
+
+function descuentos (total) {
     if (total > 20000) {
-        discount = total * 0.2
-        total = total - discount
+        descuento = total * 0.2
+        total = total - descuento
     } else if (total <= 20000 && total > 10000) {
-        discount = total * 0.15
-        total = total - discount
+        descuento = total * 0.15
+        total = total - descuento
     } else {
-        discount = total * 0.1
-        total = total - discount
+        descuento = total * 0.1
+        total = total - descuento
     }
     return total
 }
 
-const addToCart = product => {
-    switch (product) {
+function opciones (opcion) {
+    switch (opcion) {
         case 1:
-            total += 5000
+            if (puma.stock > 0) {
+            alert(`el precio es de: $${puma.precio} y el stock es de: ${puma.stock}`)
+            let comprar = prompt ("¿desea comprar el producto? (si/no):")
+            if (comprar === "si"){
+                total += puma.precio
+                alert (`añadido al carrito`)
+                puma.stock = puma.stock - 1
+               }
+            }
+            else{
+                alert (`no hay stock del producto`)
+            }
             break;
         case 2:
-            total += 8000
+            if (adidas.stock > 0) {
+                alert(`el precio es de: $${adidas.precio} y el stock es de: ${adidas.stock}`)
+                let comprar = prompt ("¿desea comprar el producto? (si/no):")
+                if (comprar === "si"){
+                    total += adidas.precio
+                    alert (`añadido al carrito`)
+                    adidas.stock = adidas.stock - 1
+                   }
+                }
+                else{
+                    alert (`no hay stock del producto`)
+                }
             break;
         case 3:
-            total += 10000
+            if (jordan.stock > 0) {
+                alert(`el precio es de: $${jordan.precio} y el stock es de: ${jordan.stock}`)
+                let comprar = prompt ("¿desea comprar el producto? (si/no):")
+                if (comprar === "si"){
+                    total += jordan.precio
+                    alert (`añadido al carrito`)
+                    jordan.stock = jordan.stock - 1
+                   }
+                }
+                else{
+                    alert (`no hay stock del producto`)
+                }
             break;
         case 4:
-            total += 7500
+            if (nike.stock > 0) {
+                alert(`el precio es de: $${nike.precio} y el stock es de: ${nike.stock}`)
+                let comprar = prompt ("¿desea comprar el producto? (si/no):")
+                if (comprar === "si"){
+                    total += nike.precio
+                    alert (`añadido al carrito`)
+                    nike.stock = nike.stock - 1
+                   }
+                }
+                else{
+                    alert (`no hay stock del producto`)
+                }
+            break;
+        case 5:
+            compra = false
             break;
     }
 }
+while (compra) {
+    
+    
+    let opcion = parseInt(prompt("Ingrese el numero de la opcion para mas informacion: 1.zapatillas Puma, 2.zapatillas Adidas, 3.zapatillas Jordan, 4.zapatillas Nike, 5.terminar compra"))
+    opciones(opcion)
 
-while (buying) {
-    let products = parseInt(prompt("Ingrese el numero del producto desea agregar al carrito: 1.Puma ($5000), 2.Adidas ($8000), 3.Jordan ($10000), 4.Nike ($7500)"))
-    addToCart(products)
-
-    let keepBuying = prompt("¿Desea seguir comprando? (si/no):")
-    while (keepBuying !== "no" && keepBuying !== "si") {
-        keepBuying = prompt("Responda: si/no")
-    }
-
-    if (keepBuying === "no") {
-        buying = false
-        total = getDiscount(total)
+   
+    if (compra === false) {
+        total = descuentos(total)
     }
 }
-
-alert(`El total de su compra es de: $${total}, el descuento aplicado es de: $${discount}`)
+alert(`El total de su compra es de: $${total}, el descuento aplicado es de: $${descuento}`)
